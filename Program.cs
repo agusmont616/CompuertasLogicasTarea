6609ef86e2075326b75205da99f52eb4729be294
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2025-09-22 10:41:09 -0300

    Circuito con 3 compuertas

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        CompuertaAND and1 = new CompuertaAND("AND-1");
+        /*CompuertaAND and1 = new CompuertaAND("AND-1");
         
         and1.AgregarEntrada("Entrada-1", 1);
         and1.AgregarEntrada("Entrada-2", 1);
@@ -25,6 +25,28 @@ class Program
         not1.AgregarEntrada("Entrada-1", 0);
         not1.Calcular();
         
-        Console.WriteLine(not1.Salida);
+        Console.WriteLine(not1.Salida);*/
+
+        CompuertaAND and1 = new CompuertaAND("AND-1");
+        CompuertaOR or2 = new CompuertaOR("OR-2");
+        CompuertaNOT not3 = new CompuertaNOT("NOT-3");
+        
+        and1.AgregarEntrada("A", 1);
+        and1.AgregarEntrada("B", 1);
+        and1.Calcular();
+        
+        Console.WriteLine(and1.Salida);
+        
+        or2.AgregarEntrada("C", 1);
+        or2.AgregarEntrada("ResultAND-1", and1.Salida);
+        or2.Calcular();
+        
+        Console.WriteLine(or2.Salida);
+        
+        not3.AgregarEntrada("ResultFinal", or2.Salida);
+        not3.Calcular();
+            
+        Console.WriteLine(not3.Salida);
+        
     }
 }
